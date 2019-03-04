1831511772bf61338dd6a11eb08f52d8e8050093
Unknown <arkhomk@yahoo.com>
2019-03-04 18:59:45 +0700

    Add Liquibase and H2

--- src/main/java/com/n2n/poc/marketdata/MarketdataApplication.java
+++ src/main/java/com/n2n/poc/marketdata/MarketdataApplication.java
@@ -2,8 +2,11 @@ package com.n2n.poc.marketdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({LiquibaseProperties.class})
 public class MarketdataApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/n2n/poc/marketdata/config/LiquibaseConfiguration.java
@@ -0,0 +1,27 @@
+package com.n2n.poc.marketdata.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class LiquibaseConfiguration {
+
+    private final Logger log = LoggerFactory.getLogger(LiquibaseConfiguration.class);
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource, LiquibaseProperties liquibaseProperties) {
+
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog(liquibaseProperties.getChangeLog());
+        log.debug("Configuring Liquibase");
+
+        return liquibase;
+    }
+}
